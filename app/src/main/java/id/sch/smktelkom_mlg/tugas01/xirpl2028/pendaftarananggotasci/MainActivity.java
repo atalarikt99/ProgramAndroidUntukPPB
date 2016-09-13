fcaf72f245b0e89d4d95571721690fa5ce32789c
taufan99 <atalarikt@gmail.com>
2016-09-13 11:17:43 +0700

    Senin, 12 September 2016 -8:55 PM: Program mulai dibuat. Hello World! diubah menjadi Selamat   datang!  -10:02 PM: Menambahkan logo (masih bentuk awal).
     Selasa, 13 September 2016
     -11:16 AM: Struktur dasar program sempurna, dengan beberapa
      pengembangan untuk menyesuaikan kebutuhan.

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl2028/pendaftarananggotasci/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl2028/pendaftarananggotasci/MainActivity.java
@@ -2,12 +2,36 @@ package id.sch.smktelkom_mlg.tugas01.xirpl2028.pendaftarananggotasci;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    EditText etNama;
+    EditText etNoReg;
+    Button bDaftar;
+    TextView tvHasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNama = (EditText) findViewById(R.id.editTextNama);
+        etNoReg = (EditText) findViewById(R.id.editTextNoReg);
+        bDaftar = (Button) findViewById(R.id.buttonDaftar);
+        bDaftar.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick (View view)
+            {
+                etNama.setText("Nama asli");
+                etNoReg.setText("No. registrasi yg tercantum pd form");
+                tvHasil.setText("Data telah dikirim, silahkan tunggu konfirmasi");
+            }
+        });
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
     }
 }
