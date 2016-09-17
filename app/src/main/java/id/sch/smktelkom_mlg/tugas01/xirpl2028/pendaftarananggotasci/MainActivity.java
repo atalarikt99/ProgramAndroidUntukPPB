e2b2fa902ed4d7e8ce260dcb6ce52ffda41fe77a
taufan99 <atalarikt@gmail.com>
2016-09-17 20:11:16 +0700

    Senin, 12 September 2016 -8:55 PM: Program mulai dibuat. Hello World! diubah menjadi Selamat   datang! -10:02 PM: Menambahkan logo (masih bentuk awal).
    Selasa, 13 September 2016
    -11:16 AM: Struktur dasar program sempurna, dengan beberapa
      pengembangan untuk menyesuaikan kebutuhan.
    
    Sabtu, 17 September 2016
    -8:09 PM: Berbagai fitur sudah mulai ditambahkan, meski masih belum
      sempurna.

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl2028/pendaftarananggotasci/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl2028/pendaftarananggotasci/MainActivity.java
@@ -5,12 +5,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
     EditText etNama;
     EditText etNoReg;
+    RadioGroup rgPendidikan;
+    Spinner spKorwil;
     Button bDaftar;
     TextView tvHasil;
 
@@ -21,17 +26,68 @@ public class MainActivity extends AppCompatActivity
 
         etNama = (EditText) findViewById(R.id.editTextNama);
         etNoReg = (EditText) findViewById(R.id.editTextNoReg);
+        rgPendidikan = (RadioGroup) findViewById(R.id.radioGroupPendidikan);
+        spKorwil = (Spinner) findViewById(R.id.spinnerKorwil);
         bDaftar = (Button) findViewById(R.id.buttonDaftar);
         bDaftar.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick (View view)
             {
-                etNama.setText("Nama asli");
-                etNoReg.setText("No. registrasi yg tercantum pd form");
-                tvHasil.setText("Data telah dikirim, silahkan tunggu konfirmasi");
+                doProccess();
             }
         });
         tvHasil = (TextView) findViewById(R.id.textViewHasil);
+    }
+
+
+    private void doProccess()
+    {
+        if(isValid())
+        {
+            tvHasil.setText("Seluruh data telah terkirim!");
+        }
+    }
+
+    private boolean isValid()
+    {
+        boolean valid = true;
+
+        String nama = etNama.getText().toString();
+        String noreg = etNoReg.getText().toString();
+        String korwil = spKorwil.getSelectedItem().toString();
+
+        if (nama.isEmpty())
+        {
+            etNama.setError("Nama lengkap belum diisi");
+            valid = false;
+        }
+        else if(nama.length()<6)
+        {
+            etNama.setError("Nama lengkap minimal 6 karakter");
+            valid = false;
+        }
+        else
+        {
+            etNama.setError(null);
+        }
+
+        if(noreg.isEmpty())
+        {
+            etNoReg.setError("No. registrasi belum diisi. Silahkan lihat formulir yang dikirimkan Bang Arkan untuk no. registrasi.");
+            valid = false;
+        }
+
+        else
+        {
+            etNoReg.setError(null);
+        }
+
+        if(korwil.isEmpty())
+        {
+            spKorwil.setPrompt();
+        }
+
+        return valid;
     }
 }
