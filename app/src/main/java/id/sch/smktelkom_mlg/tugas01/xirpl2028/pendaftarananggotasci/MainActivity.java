b321e0a99b89fb481649f59b94190c8bbbf8caaf
taufan99 <atalarikt@gmail.com>
2016-09-17 21:59:23 +0700

    Senin, 12 September 2016 -8:55 PM: Program mulai dibuat. Hello World! diubah menjadi Selamat   datang! -10:02 PM: Menambahkan logo (masih bentuk awal).
    Selasa, 13 September 2016
    -11:16 AM: Struktur dasar program sempurna, dengan beberapa
      pengembangan untuk menyesuaikan kebutuhan.
    
    Sabtu, 17 September 2016
    -8:09 PM: Berbagai fitur sudah mulai ditambahkan, meski masih belum
      sempurna.
    -9:56 PM: Pengerjaan ulang, agar program sesuai dengan yang
      diperintahkan.

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl2028/pendaftarananggotasci/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl2028/pendaftarananggotasci/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -15,9 +16,11 @@ public class MainActivity extends AppCompatActivity
     EditText etNama;
     EditText etNoReg;
     RadioGroup rgPendidikan;
-    Spinner spKorwil;
+    CheckBox cb1;
+    CheckBox cb2;
+    CheckBox cb3;
     Button bDaftar;
-    TextView tvHasil;
+    TextView tvHasil1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,67 +30,21 @@ public class MainActivity extends AppCompatActivity
         etNama = (EditText) findViewById(R.id.editTextNama);
         etNoReg = (EditText) findViewById(R.id.editTextNoReg);
         rgPendidikan = (RadioGroup) findViewById(R.id.radioGroupPendidikan);
-        spKorwil = (Spinner) findViewById(R.id.spinnerKorwil);
+        cb1 = (CheckBox) findViewById(R.id.checkBox1);
+        cb2 = (CheckBox) findViewById(R.id.checkBox2);
+        cb3 = (CheckBox) findViewById(R.id.checkBox3);
         bDaftar = (Button) findViewById(R.id.buttonDaftar);
         bDaftar.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick (View view)
             {
-                doProccess();
+                String nama = etNama.getText().toString();
+                String noreg = etNoReg.getText().toString();
+                tvHasil1.setText(nama + "dengan no. reg. " +noreg);
             }
         });
-        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+        tvHasil1 = (TextView) findViewById(R.id.textViewHasil1);
     }
 
-
-    private void doProccess()
-    {
-        if(isValid())
-        {
-            tvHasil.setText("Seluruh data telah terkirim!");
-        }
-    }
-
-    private boolean isValid()
-    {
-        boolean valid = true;
-
-        String nama = etNama.getText().toString();
-        String noreg = etNoReg.getText().toString();
-        String korwil = spKorwil.getSelectedItem().toString();
-
-        if (nama.isEmpty())
-        {
-            etNama.setError("Nama lengkap belum diisi");
-            valid = false;
-        }
-        else if(nama.length()<6)
-        {
-            etNama.setError("Nama lengkap minimal 6 karakter");
-            valid = false;
-        }
-        else
-        {
-            etNama.setError(null);
-        }
-
-        if(noreg.isEmpty())
-        {
-            etNoReg.setError("No. registrasi belum diisi. Silahkan lihat formulir yang dikirimkan Bang Arkan untuk no. registrasi.");
-            valid = false;
-        }
-
-        else
-        {
-            etNoReg.setError(null);
-        }
-
-        if(korwil.isEmpty())
-        {
-            spKorwil.setPrompt();
-        }
-
-        return valid;
-    }
 }
