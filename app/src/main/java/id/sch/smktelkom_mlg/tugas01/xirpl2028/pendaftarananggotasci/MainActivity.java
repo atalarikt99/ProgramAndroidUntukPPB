662e68c705e09c3f92a8cc4eb9570873c54ae7a6
taufan99 <atalarikt@gmail.com>
2016-09-19 11:29:09 +0700

    Senin, 12 September 2016 -8:55 PM: Program mulai dibuat. Hello World! diubah menjadi Selamat   datang! -10:02 PM: Menambahkan logo (masih bentuk awal).
    Selasa, 13 September 2016
    -11:16 AM: Struktur dasar program sempurna, dengan beberapa
      pengembangan untuk menyesuaikan kebutuhan.
    
    Sabtu, 17 September 2016
    -8:09 PM: Berbagai fitur sudah mulai ditambahkan, meski masih belum
      sempurna.
    -9:56 PM: Pengerjaan ulang, agar program sesuai dengan yang
      diperintahkan.
    
    Senin, 19 September 2016
    -5:15 AM: Kode dikerjakan ulang, tetapi tidak tereksekusi tanpa alasan
      jelas.
    -11:28 AM: Kode kini dapat dieksekusi, program selesai ditulis.

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl2028/pendaftarananggotasci/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl2028/pendaftarananggotasci/MainActivity.java
@@ -1,6 +1,5 @@
 package id.sch.smktelkom_mlg.tugas01.xirpl2028.pendaftarananggotasci;
 
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,26 +11,28 @@ import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity
 {
     EditText etNama;
     EditText etNoReg;
     RadioGroup rgPendidikan;
-    CheckBox cb1;
-    CheckBox cb2;
-    CheckBox cb3;
+    Spinner spKorwil;
+    CheckBox cb1, cb2, cb3;
     Button bDaftar;
-    TextView tvHasil1;
-    TextView tvHasil2;
+    TextView tvHasil1, tvHasil2, tvHasil3, tvHasil4;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         etNama = (EditText) findViewById(R.id.editTextNama);
         etNoReg = (EditText) findViewById(R.id.editTextNoReg);
         rgPendidikan = (RadioGroup) findViewById(R.id.radioGroupPendidikan);
+        spKorwil = (Spinner) findViewById(R.id.spinnerKorwil);
         cb1 = (CheckBox) findViewById(R.id.checkBox1);
         cb2 = (CheckBox) findViewById(R.id.checkBox2);
         cb3 = (CheckBox) findViewById(R.id.checkBox3);
@@ -41,15 +42,54 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick (View view)
             {
-                String nama = etNama.getText().toString();
-                String noreg = etNoReg.getText().toString();
-                tvHasil1.setText("Identitas: " + nama + " dengan no. reg. " +noreg);
-                doClick();
-
+                if(isValid())
+                {
+                    String nama = etNama.getText().toString();
+                    String noreg = etNoReg.getText().toString();
+                    tvHasil1.setText("Identitas: " + nama + " dengan no. reg. " + noreg);
+                    doClick();
+                }
             }
         });
         tvHasil1 = (TextView) findViewById(R.id.textViewHasil1);
         tvHasil2 = (TextView) findViewById(R.id.textViewHasil2);
+        tvHasil3 = (TextView) findViewById(R.id.textViewHasil3);
+        tvHasil4 = (TextView) findViewById(R.id.textViewHasil4);
+    }
+
+    private boolean isValid()
+    {
+        boolean valid = true;
+
+        String nama = etNama.getText().toString();
+        String noreg = etNoReg.getText().toString();
+
+        if(nama.isEmpty())
+        {
+            etNama.setError("Nama lengkap belum diisi");
+            valid = false;
+        }
+        else if(nama.length()<6)
+        {
+            etNama.setError("Nama lengkap minimal 6 karakter");
+            valid = false;
+        }
+        else
+        {
+            etNama.setError(null);
+        }
+
+        if(noreg.isEmpty())
+        {
+            etNoReg.setError("No. registrasi belum diisi");
+            valid = false;
+        }
+        else
+        {
+            etNoReg.setError(null);
+        }
+
+        return valid;
     }
 
     private void doClick()
@@ -70,6 +110,18 @@ public class MainActivity extends AppCompatActivity
         {
             tvHasil2.setText("Pendidikan terakhir: " + hasil);
         }
+
+        tvHasil3.setText("Korwil: " + spKorwil.getSelectedItem().toString());
+
+        String hasil2 = "Peraturan yang dipatuhi:\n";
+        int startlen = hasil2.length();
+        if(cb1.isChecked()) hasil2+=cb1.getText()+"\n";
+        if(cb2.isChecked()) hasil2+=cb2.getText()+"\n";
+        if(cb3.isChecked()) hasil2+=cb3.getText()+"\n";
+
+        if(hasil2.length()==startlen) hasil2+="Belum mematuhi aturan satu pun";
+
+        tvHasil4.setText(hasil2);
     }
 
 }
